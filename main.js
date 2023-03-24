1e7812bfc0e349f551ce2bc958efaa4e93b1513d
DrizztDuncan <duncantheinvictus@gmail.com>
2023-03-25 00:14:10 +0800

    not flipping yet

--- main.js
+++ main.js
@@ -7,19 +7,58 @@ const Symbols = [
 
 const view = {
   getCardElement(index) {
-    const number = (index % 13) + 1;
+    return `<div class="card back"></div>`;
+  },
+  getCardContent(index) {
+    const number = this.transformNumber((index % 13) + 1);
     const symbol = Symbols[Math.floor(index / 13)];
     return `
-        <div class="card">
-          <p>6</p>
-          <img src="https://assets-lighthouse.alphacamp.co/uploads/image/file/17992/heart.png">
-          <p>6</p>
-        </div>
-      `;
+      <p>${number}</p>
+      <img src="${symbol}" />
+      <p>${number}</p>
+    `;
+  },
+  transformNumber(number) {
+    switch (number) {
+      case 1:
+        return "A";
+      case 11:
+        return "J";
+      case 12:
+        return "Q";
+      case 13:
+        return "K";
+      default:
+        return number;
+    }
   },
   displayCards() {
     const rootElement = document.querySelector("#cards");
-    rootElement.innerHTML = this.getCardElement();
+    rootElement.innerHTML = utility
+      .getRandomNumberArray(52)
+      .map((index) => this.getCardElement(index))
+      .join("");
+  },
+};
+
+const utility = {
+  getRandomNumberArray(count) {
+    const number = Array.from(Array(count).keys());
+    for (let index = number.length - 1; index > 0; index--) {
+      let randomIndex = Math.floor(Math.random() * (index + 1));
+      [number[index], number[randomIndex]] = [
+        number[randomIndex],
+        number[index],
+      ];
+    }
+    return number;
   },
 };
 view.displayCards();
+
+// Node list (array-like)
+document.querySelectorAll(".card").forEach((card) => {
+  card.addEventListener("click", (event) => {
+    console.log(card);
+  });
+});
