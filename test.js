f0ee2073e31510334835f30144093cc2fccb2433
DrizztDuncan <duncantheinvictus@gmail.com>
2023-03-28 15:11:37 +0800

    Done

--- test.js
+++ test.js
@@ -5,14 +5,12 @@ const GAME_STATE = {
   CardsMatched: "CardsMatched",
   GameFinished: "GameFinished",
 };
-
 const Symbols = [
   "https://assets-lighthouse.alphacamp.co/uploads/image/file/17989/__.png", // 黑桃
   "https://assets-lighthouse.alphacamp.co/uploads/image/file/17992/heart.png", // 愛心
   "https://assets-lighthouse.alphacamp.co/uploads/image/file/17991/diamonds.png", // 方塊
   "https://assets-lighthouse.alphacamp.co/uploads/image/file/17988/__.png", // 梅花
 ];
-
 const view = {
   getCardElement(index) {
     return `<div data-index="${index}" class="card back"></div>`;
@@ -21,10 +19,10 @@ const view = {
     const number = this.transformNumber((index % 13) + 1);
     const symbol = Symbols[Math.floor(index / 13)];
     return `
-      <p>${number}</p>
-      <img src="${symbol}" />
-      <p>${number}</p>
-    `;
+        <p>${number}</p>
+        <img src="${symbol}" />
+        <p>${number}</p>
+      `;
   },
   transformNumber(number) {
     switch (number) {
@@ -46,54 +44,21 @@ const view = {
       .map((index) => this.getCardElement(index))
       .join("");
   },
-  flipCards(...cards) {
-    cards.map((card) => {
-      if (card.classList.contains("back")) {
-        card.classList.remove("back");
-        card.innerHTML = this.getCardContent(Number(card.dataset.index));
-        return;
-      }
-      card.classList.add("back");
-      card.innerHTML = null;
-    });
+  flipCard(card) {
+    if (card.classList.contains("back")) {
+      // 回傳正面
+      card.classList.remove("back");
+      card.innerHTML = this.getCardContent(Number(card.dataset.index));
+      return;
+    }
+    // 回傳背面
+    card.classList.add("back");
+    card.innerHTML = null;
   },
-  pairCards(...cards) {
-    cards.map((card) => {
-      card.classList.add("paired");
-    });
-  },
-  renderScore(score) {
-    document.querySelector(".score").textContent = `Score: ${score}`;
-  },
-  renderTriedTimes(times) {
-    document.querySelector(
-      ".tried"
-    ).textContent = `You've tried: ${times} times`;
-  },
-  appendWrongAnimation(...cards) {
-    cards.map((card) => {
-      card.classList.add("wrong");
-      card.addEventListener(
-        "animationend",
-        (event) => event.target.classList.remove("wrong"),
-        { once: true }
-      );
-    });
-  },
-
-  showGameFinished() {
-    const div = document.createElement("div");
-    div.classList.add("completed");
-    div.innerHTML = `
-      <p>Complete!</p>
-      <p>Score: ${model.score}</p>
-      <p>You've tried: ${model.triedTimes} times</p>
-    `;
-    const header = document.querySelector("#header");
-    header.before(div);
+  pairCard(card) {
+    card.classList.add("paired");
   },
 };
-
 const model = {
   revealedCards: [],
   isRevealedCardsMatched() {
@@ -102,10 +67,7 @@ const model = {
       this.revealedCards[1].dataset.index % 13
     );
   },
-  score: 0,
-  triedTimes: 0,
 };
-
 const controller = {
   currentState: GAME_STATE.FirstCardAwaits,
   generateCards() {
@@ -117,39 +79,31 @@ const controller = {
     }
     switch (this.currentState) {
       case GAME_STATE.FirstCardAwaits:
-        view.flipCards(card);
+        view.flipCard(card);
         model.revealedCards.push(card);
         this.currentState = GAME_STATE.SecondCardAwaits;
         break;
       case GAME_STATE.SecondCardAwaits:
-        view.renderTriedTimes(++model.triedTimes);
-        view.flipCards(card);
+        view.flipCard(card);
         model.revealedCards.push(card);
         // 判斷配對是否成功
         if (model.isRevealedCardsMatched()) {
           // 配對成功
-          view.renderScore((model.score += 10));
           this.currentState = GAME_STATE.CardsMatched;
-          view.pairCards(...model.revealedCards);
+          view.pairCard(model.revealedCards[0]);
+          view.pairCard(model.revealedCards[1]);
           model.revealedCards = [];
-          if (model.score === 260) {
-            console.log("showGameFinished");
-            this.currentState = GAME_STATE.GameFinished;
-            view.showGameFinished(); // 加在這裡
-            return;
-          }
           this.currentState = GAME_STATE.FirstCardAwaits;
         } else {
           // 配對失敗
           this.currentState = GAME_STATE.CardsMatchFailed;
-          view.appendWrongAnimation(...model.revealedCards);
           setTimeout(() => {
-            view.flipCards(...model.revealedCards);
+            view.flipCard(model.revealedCards[0]);
+            view.flipCard(model.revealedCards[1]);
             model.revealedCards = [];
             this.currentState = GAME_STATE.FirstCardAwaits;
           }, 1000);
         }
-        // 判斷配對是否成功
         break;
     }
     console.log("this.currentState", this.currentState);
@@ -158,13 +112,7 @@ const controller = {
       model.revealedCards.map((card) => card.dataset.index)
     );
   },
-  resetCards() {
-    view.flipCards(...model.revealedCards);
-    model.revealedCards = [];
-    controller.currentState = GAME_STATE.FirstCardAwaits;
-  },
 };
-
 const utility = {
   getRandomNumberArray(count) {
     const number = Array.from(Array(count).keys());
@@ -178,10 +126,7 @@ const utility = {
     return number;
   },
 };
-
-controller.generateCards(); // 取代 view.displayCards()
-
-// Node list (array-like)
+controller.generateCards();
 document.querySelectorAll(".card").forEach((card) => {
   card.addEventListener("click", (event) => {
     controller.dispatchCardAction(card);
